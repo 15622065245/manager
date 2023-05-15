054c81aeef04460db95d101d07d8ed8956dc34ed
妄定 <9205024+false-determination@user.noreply.gitee.com>
2023-05-15 21:57:07 +0800

    添加设置权限路由

--- src/project/router/role.js
+++ src/project/router/role.js
@@ -20,6 +20,30 @@ const role = {
             },
             component: () => import("@/project/views/role/list"),
         },
+        {
+            path: "/role/show/:id",
+            name: "roleShow",
+            meta: {
+                isShowHeader: true,
+                isShowLeftSider: true,
+                keepAlive: false,
+                breadcrumb: [
+                    {
+                        name: "系统管理",
+                        path: "",
+                    },
+                    {
+                        name: "角色权限",
+                        path: "/role/list",
+                    },
+                    {
+                        name: "权限设置",
+                        path: "/role/show/:id",
+                    },
+                ],
+            },
+            component: () => import("@/project/views/role/show"),
+        },
     ],
 }
 export default role
